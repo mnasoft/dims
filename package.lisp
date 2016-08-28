e8b86e2af589285db3bd2054d41cba470f2d8a92
Nick Matvyeyev <mnasoft@gmail.com>
2016-08-28 17:37:46 +0200

    2016-8-28 17:37:30 mnasoft-pi

--- package.lisp
+++ package.lisp
@@ -2,12 +2,7 @@
 
 (defpackage #:dims
   (:use #:cl)
-  (:export K->C)			; Перевод градусов кельвина в градусы цельсия
-  (:export C->K)			; Перевод градусов цельсия в градусы кельвина
-  (:export k->M)			; Перевод значения с приставкой кило в число с приставкой мега
-  (:export M->k)			; Перевод значения с приставкой мега в число с приставкой кило
-  (:export kgs/cm2->Pa)			;
-  (:export Pa->kgs/cm2)			;
+
   )
 
 ;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
